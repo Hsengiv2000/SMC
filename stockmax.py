ff63ed92a3a4e5a7db6f3baa31ab42c8d31a16f0
Hsengiv2000 <46417635+Hsengiv2000@users.noreply.github.com>
2020-05-05 23:12:09 +0800

    Create stockmax.py

---
+++ stockmax.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'stockmax' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts INTEGER_ARRAY prices as parameter.
+#
+
+def stockmax(prices):
+    profit=0
+    highestSoFar=0
+   
+    for i in prices[::-1]:
+        highestSoFar = max(i, highestSoFar)
+        profit+=highestSoFar- i  #Strategy is to sell at all the low points when iterating backwards
+    return profit
+
+        
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        prices = list(map(int, input().rstrip().split()))
+
+        result = stockmax(prices)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
